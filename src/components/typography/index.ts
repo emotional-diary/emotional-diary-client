7ca50e5deedf9ab0158075a2bf135d8b48246d1c
msg824 <msg824@naver.com>
2023-04-18 00:00:35 +0900

    feat: 로그인 및 회원가입 UI 임시 구현

---
+++ src/components/typography/index.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const Typography = {
+  h1: styled.h1`
+    font-size: 2.5rem;
+    font-weight: 700;
+    line-height: 1.2;
+  `,
+  h2: styled.h2`
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 1.2;
+  `,
+  h3: styled.h3`
+    font-size: 1.75rem;
+    font-weight: 600;
+    line-height: 1.2;
+  `,
+  body1: styled.div`
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+  `,
+  body2: styled.div`
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+  `,
+};
+
+export { Typography };
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Container } from '@components/layout';
+import { SignUpForm } from '@components/form';
+
+export default function SignUp() {
+  return (
+    <Container>
+      <SignUpForm />
+    </Container>
+  );
+}
