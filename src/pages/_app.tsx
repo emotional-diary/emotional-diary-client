d397afd8650d3d308ec90a7542e05e6afc8e5718
msg824 <msg824@naver.com>
2023-05-21 20:06:36 +0900

    feat: manifest 세팅 및 스플래시 이미지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import "../styles/globals.css";
-import { AppProps } from "next/app";
+import Head from 'next/head';
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -20,23 +20,60 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="keywords"
           content="Emotional Diary, Ai, online diary, web diary, diary app"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+
         <title>Emotional Diary</title>
 
         <link rel="manifest" href="/manifest.json" />
+
+        {/* 파비콘 */}
         <link
-          href="/icons/favicon-16x16.png"
+          href="/images/favicons/favicon-16x16.ico"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/images/favicons/favicon-32x32.ico"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+
+        {/* 스플래시 */}
+        <link
+          href="/images/splashscreens/iphone5_splash.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/images/splashscreens/iphone6_splash.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/images/splashscreens/iphoneplus_splash.png"
+          media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/images/splashscreens/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/images/splashscreens/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/images/splashscreens/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* 브라우저 테마 */}
+        <meta name="theme-color" content="#2B625B" />
       </Head>
       <Component {...pageProps} />
     </>
