2a85b290dab416ea8b77ea01dbec205baa1df90f
msg824 <msg824@naver.com>
2023-08-08 16:44:24 +0900

    feat: react-query provider 추가 및 복수의 provider 하나로 통합

--- app/client/theme.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { ThemeProvider } from 'styled-components';
-
-import { theme } from 'src/theme';
-
-export default ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
