7d07bfddbfc7395cd5420ab4ab96750202a8045e
msg824 <msg824@naver.com>
2023-08-18 00:50:02 +0900

    feat: 웹 서버 API 추가
    일기 조회, 상세 조회, 등록, 수정, 삭제\n유저 정보 조회\n비밀번호 검증, 변경

--- app/api/user/password/change/route.ts
+++ app/api/user/password/change/route.ts
@@ -1,48 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function GET(request: NextRequest) {
-  try {
-    const accessToken =
-      request.headers.get('authorization') ??
-      request.cookies.get('accessToken')?.value;
-
-    if (!accessToken) {
-      return NextResponse.json(
-        {
-          message: 'Unauthorized',
-        },
-        {
-          status: 401,
-          headers: {
-            'Set-Cookie': `accessToken=;Max-Age=0;HttpOnly;Secure;Path=/`,
-          },
-        }
-      );
-    }
-
-    const res = await axios.get(`${process.env.SERVER_HOST}/v1/users`, {
-      headers: {
-        authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    return NextResponse.json(res.data);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      return NextResponse.json(error.response?.data, {
-        status: error.response?.status,
-      });
-    } else {
-      console.error(`${__dirname} error`, error);
-
-      return NextResponse.json(error, {
-        status: 500,
-      });
-    }
-  }
-}
-
 export async function PATCH(request: NextRequest) {
   try {
     const req = await request.json();
@@ -63,9 +21,9 @@ export async function PATCH(request: NextRequest) {
     }
 
     const res = await axios.patch(
-      `${process.env.SERVER_HOST}/v1/users`,
+      `${process.env.SERVER_HOST}/v1/users/change-pwd`,
       {
-        ...req,
+        newPassword: req.newPassword,
       },
       {
         headers: {
