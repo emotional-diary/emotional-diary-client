e6b427cb445f75c88eb37e8788f7bd193cd1c414
msg824 <msg824@naver.com>
2024-01-13 00:38:55 +0900

    refactor: 회원가입 완료 페이지 single page로 변경

--- src/app/signup/client.tsx
+++ src/app/signup/client.tsx
@@ -5,15 +5,22 @@ import React from 'react';
 import { Container } from '@components/layout';
 import { SignUpForm } from '@components/form';
 import { SignUpProps } from './page';
+import SignUpComplete from './complete';
 
 export default function SignUpPage({ props }: { props: SignUpProps }) {
+  const [isComplete, setIsComplete] = React.useState(false);
+
+  if (isComplete) {
+    return <SignUpComplete />;
+  }
+
   return (
     <Container
       headerProps={{
         back: true,
       }}
     >
-      <SignUpForm {...props} />
+      <SignUpForm setIsComplete={setIsComplete} {...props} />
     </Container>
   );
 }
