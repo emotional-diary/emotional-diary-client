8a933f0d327b2bdccac1adab9c089a3aa60ec517
msg824 <msg824@naver.com>
2024-03-22 01:59:59 +0900

    fix: 일기 작성 완료 시 로딩 깜빡임 fix

--- src/app/diary/new/page.tsx
+++ src/app/diary/new/page.tsx
@@ -87,7 +87,7 @@ export default function NewDiary() {
     onMutate: () => {
       setIsLoading(true);
     },
-    onSettled: () => {
+    onError: () => {
       setIsLoading(false);
     },
   });
